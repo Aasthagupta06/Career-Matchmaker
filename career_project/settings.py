6fe47f6a9026b0795ff3b5379b67b7975d042e2c
Aastha Gupta <aasthaworks27@gmail.com>
2025-07-08 20:07:50 +0530

    Update settings.py

--- career_project/settings.py
+++ career_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'career_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'career_db',
+        'USER': 'career_user',
+        'PASSWORD': 'Saimandir',  # use the password you set in pgAdmin
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
