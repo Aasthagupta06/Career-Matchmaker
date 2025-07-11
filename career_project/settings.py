20c2239be66c77aedbc48bebf1472e6420132cd8
Aastha Gupta <aasthaworks27@gmail.com>
2025-07-11 23:42:41 +0530

    Update settings.py

--- career_project/settings.py
+++ career_project/settings.py
@@ -80,14 +80,27 @@ WSGI_APPLICATION = 'career_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'career_db',
+#         'USER': 'career_user',
+#         'PASSWORD': 'Saimandir',  # use the password you set in pgAdmin
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'career_db',
-        'USER': 'career_user',
-        'PASSWORD': 'Saimandir',  # use the password you set in pgAdmin
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DJANGO_DB_NAME', 'career_db'),
+        'USER': os.environ.get('DJANGO_DB_USER', 'career_user'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'Saimandir'),
+        'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DJANGO_DB_PORT', '5432'),
     }
 }
 
@@ -126,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
